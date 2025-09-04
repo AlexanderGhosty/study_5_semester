497d40c88328aece76ef7563593674cdf42fc57d
Alexander Bykov <113583500+AlexanderGhosty@users.noreply.github.com>
2025-09-04 19:21:50 +0300

    backend/l1

---
+++ backend/l1/main.py
@@ -0,0 +1,12 @@
+"""Лабораторная работа №1."""
+
+def hello_word() -> None:
+    """Печатает приветствие"""
+    print("Hello Word")
+
+def main() -> None:
+    """Точка входа"""
+    hello_word()
+
+if __name__ == "__main__":
+    main()
